8c13606e094ba6b6ee43a2c87612adf7d93134ec
wsfreund <wsfreund@gmail.com>
2009-08-25 15:38:02 -0300

    genData->genCompData

--- loadGenData.C
+++ loadGenData.C
@@ -3,5 +3,5 @@ gROOT->ProcessLine(".L Efic.cxx+");
 gROOT->ProcessLine(".L T2CaloEfic.cxx+");
 gROOT->ProcessLine(".L Neural.cxx+");
 gROOT->ProcessLine(".L NeuralEfic.cxx+");
-gROOT->ProcessLine(".L genData.C+");
+gROOT->ProcessLine(".L genCompData.C+");
 }
