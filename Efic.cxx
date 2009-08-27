5a97d2a78a7503de7a60dc5c9201ef1a3250c271
wsfreund <wsfreund@gmail.com>
2009-08-27 17:12:47 -0300

    Colocado os tabs para 4 espacos

--- Efic.cxx
+++ Efic.cxx
@@ -3,34 +3,33 @@
 Efic::CODE Efic::ordenateRoi(const vector<float> *eta, const vector<float> *phi){
 
 
-        for(size_t j=0; j<lvl2_eta->size();++j){
+    for(size_t j=0; j<lvl2_eta->size();++j){
+        if ( j < eta->size() ){
 
-                if ( j < eta->size() ){
-	                unsigned matchingPair	        = 	j;
-	                float	vError			=	sqrt (pow(( eta->at(j) - lvl2_eta->at(j) ),2)+pow(( phi->at(j) - lvl2_phi->at(j) ),2) );
+            unsigned matchingPair	        = 	j;
+            float	vError			=	sqrt (pow(( eta->at(j) - lvl2_eta->at(j) ),2)+pow(( phi->at(j) - lvl2_phi->at(j) ),2) );
 
 
-	                for(size_t k= (j+1) ; k < (lvl2_eta->size()); ++k){
+        for(size_t k= (j+1) ; k < (lvl2_eta->size()); ++k){
 
-		                float actualError =  sqrt (pow(( eta->at(j) - lvl2_eta->at(k) ),2)+pow(( phi->at(j) - lvl2_phi->at(k) ),2) );
-
-		                if ( actualError < vError ) {
-			                vError = actualError;
-			                matchingPair = k;
-		                }
-	                }
-
-                        if (j!=matchingPair) {
-                                swapVectors(j,matchingPair);
-                        }
-                }
-
-                else {
-                        eraseVectors(j);
-                        break;
-                }
-
+            float actualError =  sqrt (pow(( eta->at(j) - lvl2_eta->at(k) ),2)+pow(( phi->at(j) - lvl2_phi->at(k) ),2) );
+            if ( actualError < vError ) {
+                vError = actualError;
+                matchingPair = k;
+            }
         }
+
+            if (j!=matchingPair) {
+                swapVectors(j,matchingPair);
+            }
+        }
+
+        else {
+                eraseVectors(j);
+                break;
+        }
+
+    }
 
 	return Efic::OK;
 }
