761c85a1a57372f19f624e4e8bc7de16c888fb85
wsfreund <wsfreund@gmail.com>
2009-12-11 00:19:18 -0200

    Colocado em namespace

--- T2CaloConfig.h
+++ T2CaloConfig.h
@@ -1,27 +1,29 @@
 //e10_medium constants v.55
+namespace t2CaloConfig{
+
 /*
-const float 	T2CaloEfic::m_eTthr[] 		=	{9.e3, 9.e3, 9.e3, 9.e3, 9.e3, 9.e3, 9.e3};
-const float 	T2CaloEfic::m_eT2thr[]		=	{90e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3};
-const float 	T2CaloEfic::m_hadeTthr[]	=	{2.0e3, 2.0e3, 2.0e3, 2.0e3, 2.0e3, 2.0e3, 2.0e3};
-const float 	T2CaloEfic::m_hadeT2thr[]	=	{999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3};
-const float 	T2CaloEfic::m_etabin[]		=	{0., 0.75, 1.37, 1.52, 1.8, 2.0, 2.35, 2.5};
-const float 	T2CaloEfic::m_carcorethr[]	=	{0.85, 0.85, 0.85, 0.85, 0.85, 0.85, 0.85};
-const float 	T2CaloEfic::m_caeratiothr[]	=	{0.60,0.60, 0.60, 0.60, 0.60, 0.60, 0.60};
-const float 	T2CaloEfic::m_F1thr		    =	(0.005);
-const float 	T2CaloEfic::m_detacluster	=	(0.1);
-const float 	T2CaloEfic::m_dphicluster	=	(0.1);
+    const float 	m_eTthr[] 		=	{9.e3, 9.e3, 9.e3, 9.e3, 9.e3, 9.e3, 9.e3};
+    const float 	m_eT2thr[]		=	{90e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3};
+    const float 	m_hadeTthr[]	=	{2.0e3, 2.0e3, 2.0e3, 2.0e3, 2.0e3, 2.0e3, 2.0e3};
+    const float 	m_hadeT2thr[]	=	{999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3};
+    const float 	m_etabin[]		=	{0., 0.75, 1.37, 1.52, 1.8, 2.0, 2.35, 2.5};
+    const float 	m_carcorethr[]	=	{0.85, 0.85, 0.85, 0.85, 0.85, 0.85, 0.85};
+    const float 	m_caeratiothr[]	=	{0.60,0.60, 0.60, 0.60, 0.60, 0.60, 0.60};
+    const float 	m_F1thr		    =	(0.005);
+    const float 	m_detacluster	=	(0.1);
+    const float 	m_dphicluster	=	(0.1);
 */
 
 //e15i_medium constants v.55
+    const float 	m_eTthr[] 		=	{14.e3, 14.e3, 14.e3, 14.e3, 14.e3, 14.e3, 14.e3};
+    const float 	m_eT2thr[]		=	{90e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3};
+    const float 	m_hadeTthr[]	=	{3.0e3, 3.0e3, 3.0e3, 3.0e3, 3.0e3, 3.0e3, 3.0e3};
+    const float 	m_hadeT2thr[]	=	{999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3};
+    const float 	m_etabin[]		=	{0., 0.75, 1.37, 1.52, 1.8, 2.0, 2.35, 2.5};
+    const float 	m_carcorethr[]	=	{0.87, 0.87, 0.87, 0.87, 0.87, 0.87, 0.87};
+    const float 	m_caeratiothr[]	=	{0.60,0.60, 0.60, 0.60, 0.60, 0.60, 0.60};
+    const float 	m_F1thr		    =	(0.005);
+    const float 	m_detacluster	=	(0.1);
+    const float 	m_dphicluster	=	(0.1);
 
-const float 	T2CaloEfic::m_eTthr[] 		=	{14.e3, 14.e3, 14.e3, 14.e3, 14.e3, 14.e3, 14.e3};
-const float 	T2CaloEfic::m_eT2thr[]		=	{90e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3, 90.0e3};
-const float 	T2CaloEfic::m_hadeTthr[]	=	{3.0e3, 3.0e3, 3.0e3, 3.0e3, 3.0e3, 3.0e3, 3.0e3};
-const float 	T2CaloEfic::m_hadeT2thr[]	=	{999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3, 999.0e3};
-const float 	T2CaloEfic::m_etabin[]		=	{0., 0.75, 1.37, 1.52, 1.8, 2.0, 2.35, 2.5};
-const float 	T2CaloEfic::m_carcorethr[]	=	{0.87, 0.87, 0.87, 0.87, 0.87, 0.87, 0.87};
-const float 	T2CaloEfic::m_caeratiothr[]	=	{0.60,0.60, 0.60, 0.60, 0.60, 0.60, 0.60};
-const float 	T2CaloEfic::m_F1thr		    =	(0.005);
-const float 	T2CaloEfic::m_detacluster	=	(0.1);
-const float 	T2CaloEfic::m_dphicluster	=	(0.1);
-
+}
