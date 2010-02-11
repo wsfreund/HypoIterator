8e04b4cc3b5e4d770c186084e7d4ff05f7522538
Werner Freund <wsfreund@gmail.com>
2010-02-11 22:26:57 +0100

    mesma coisa do commit anterior que eu esqueci de commitar

--- EtEtaPhi/T2CaEtEtaPhi.cxx
+++ EtEtaPhi/T2CaEtEtaPhi.cxx
@@ -1,24 +1,26 @@
 #include "T2CaEtEtaPhi.h"
 
-T2CaEtEtaPhi::T2CaEtEtaPhi(const std::string &rPath,const unsigned int user_nRegions, const std::string &userDataLabel):
+T2CaEtEtaPhi::T2CaEtEtaPhi(std::string &rPath, unsigned user_nRegions, std::string &userDataLabel):
     T2CaloCommon(rPath),
     NREGIONS(user_nRegions),
     dataLabel(userDataLabel){
 
-    etaxEficGraph(-2.5,2.5,extraVariables,"T2CaEta", NREGIONS, dataLabel, "T2CaEfic x Eta" );
-    phixEficGraph(-TMath::pi, TMath::pi,extraVariables,"T2CaPhi", NREGIONS, dataLabel, "T2CaPhi x Phi" );
-    etxEficGraph( 10e3, 80e3,extraVariables,"T2CaEt", NREGIONS, dataLabel, "T2CaEfic x Et");
+
+    HypoErrorsGraph lala(-2.5, 2.5, extraVariables, "T2CaEta", NREGIONS, dataLabel, "T2CaEfic x Eta");
+    etaxEficGraph = new HypoErrorsGraph(-2.5, 2.5, extraVariables, "T2CaEta", NREGIONS, dataLabel, "T2CaEfic x Eta");
+    phixEficGraph = new HypoErrorsGraph(-TMath::Pi(), TMath::Pi(), extraVariables, "T2CaPhi", NREGIONS, dataLabel, "T2CaEfic x Phi");
+    etxEficGraph = new HypoErrorsGraph(10e3, 80e3, extraVariables, "T2CaEt", NREGIONS, dataLabel, "T2CaEfic x Et");
 
 }
 
 HypoBase::CODE T2CaEtEtaPhi::DrawEficVs(const std::string &vsWho, const std::string &opt){
 
     if ( vsWho == "eta")
-        etaxEficGraph->Draw(opt)
+        etaxEficGraph->Draw(opt);
     else if (vsWho == "phi")
-        phixEficGraph->Draw(opt)
+        phixEficGraph->Draw(opt);
     else if (vsWho == "et")
-        etxEficGraph->Draw(opt)
+        etxEficGraph->Draw(opt);
 
     return HypoBase::OK;
 
