c0c3f44b1f203464cdaa4b534d6ffb9cc11e1528
Werner Freund <wsfreund@gmail.com>
2010-02-12 01:23:50 +0100

    tinha colocado *error

--- EtEtaPhi/HypoErrorsGraph.cxx
+++ EtEtaPhi/HypoErrorsGraph.cxx
@@ -130,7 +130,7 @@ HypoErrorsGraph::CODE HypoErrorsGraph::genEficErrors(const float* edges, float* 
             if (error>*efic) error = *efic;
             *lowEdgeErrors = *efic - error; 
             *hiEdgeErrors = ((*efic + error) > 100)?100:(*efic+ error); 
-            cout<<*efic<<"     "<<*error<<"    "<<*lowEdgeErrors<<"     "<<*hiEdgeErrors<<endl;
+            cout<<*efic<<"     "<<error<<"    "<<*lowEdgeErrors<<"     "<<*hiEdgeErrors<<endl;
         }
     }else{
         for(unsigned lowEdge = 0; lowEdge < NREGIONS; ++lowEdge, ++efic, ++lowEdgeErrors, ++hiEdgeErrors){
