da62fdbb17dfe8ae6442e7204c76f42c6d109407
Werner Freund <wsfreund@gmail.com>
2010-02-13 19:50:26 +0100

    Mudei o nome do T2CaEtEtaPhi

--- compile_EtEtaPhi.C
+++ compile_EtEtaPhi.C
@@ -2,5 +2,5 @@
 gROOT->ProcessLine(".L HypoBase.h++");
 gROOT->ProcessLine(".L T2CaCommon.cxx++");
 gROOT->ProcessLine(".L EtEtaPhi/HypoErrorsGraph.cxx++");
-gROOT->ProcessLine(".L EtEtaPhi/T2CaEtEtaPhi.cxx++");
+gROOT->ProcessLine(".L EtEtaPhi/T2CaRelEfic.cxx++");
 }
