a43212cb18bd68aef168677758f0e26ca36a6f0c
Werner Freund <wsfreund@gmail.com>
2010-02-13 22:14:51 +0100

    teste tirando a referencia

--- HypoBase.h
+++ HypoBase.h
@@ -54,7 +54,7 @@ class HypoBase {
         const std::vector<float> *getPhi(){  return lvl2_phi; }
         const std::vector<int>   *getDec(){  return decision; }
         const std::vector<float> *getEt() {  return et;       }
-        void getExtraVariables(const TTree*& refExtraVariables){ refExtraVariables = extraVariables; };
+        void getExtraVariables(const TTree* refExtraVariables){ refExtraVariables = extraVariables; };
 
         virtual CODE exec() = 0;
 
