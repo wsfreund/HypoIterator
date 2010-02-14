2632fdd4970624c8e2853aa93a06ee3844c7397f
Werner Freund <wsfreund@gmail.com>
2010-02-14 18:03:32 +0100

    pausa porque chegou visita

--- EtEtaPhi/T2CaRelEfficCanvas.cxx
+++ EtEtaPhi/T2CaRelEfficCanvas.cxx
@@ -41,11 +41,11 @@ HypoBase::CODE T2CaRelEfficCanvas::Draw(){
             float detHadEtRate = pHypo->getDetEtRate();
             std::string dataLabel 
             pHypo->getDataLabel(dataLabel);
-            line2.Form("T2Calo Efficience = %.4f", detRate);
-            line3.Form("rCore Cut Efficience = %.4f", detrCoreRate);
-            line4.Form("eRatio Cut Efficience = %.4f", deteRatioRate);
-            line5.Form("Et_{Em} Cut Efficience = %.4f", detEtRate);
-            line6.Form("Et_{Had} Cut Efficience = %.4f", detHadEtRate;);
+            line2.Form("T2Calo Detection Rate = %.4f", detRate);
+            line3.Form("rCore Cut Detection Rate = %.4f", detrCoreRate);
+            line4.Form("eRatio Cut Detection Rate = %.4f", deteRatioRate);
+            line5.Form("Et_{Em} Cut Detection Rate = %.4f", detEtRate);
+            line6.Form("Et_{Had} Cut Detection Rate = %.4f", detHadEtRate;);
             pt->AddText("");
             pt->AddText(line1);
             pt->AddText("");
@@ -70,19 +70,31 @@ HypoBase::CODE T2CaRelEfficCanvas::Draw(){
         line1.Form("Total Data = %d:Electrons = %d:Jets = %d",totalData, totalDataElc,totalDataJet);
         if (pHypo){
             TString line2, line3, line4, line5, line6, line7;
-            float detrCoreRate = pHypo->getDetrCoreRate();
-            float deteRatioRate = pHypo->getDeteRatioRate();
-            float detEtRate = pHypo->getDetEtRate();
-            float detHadEtRate = pHypo->getDetEtRate();
+            float detrCoreRateElc = pHypoElc->getDetrCoreRate();
+            float deteRatioRateElc = pHypoElc->getDeteRatioRate();
+            float detEtRateElc = pHypoElc->getDetEtRate();
+            float detHadEtRateElc = pHypoElc->getDetEtRate();
+            float detrCoreRateJet = pHypoJet->getDetrCoreRate();
+            float deteRatioRateJet = pHypoJet->getDeteRatioRate();
+            float detEtRateJet = pHypoJet->getDetEtRate();
+            float detHadEtRateJet = pHypoJet->getDetEtRate();
+            float rCoreEffic = calcSP(detrCoreRateElc, detrCoreRateJet);
+            float eRatioEffic = calcSP(deteRatioRateElc, deteRatioRateJet);
+            float etEffic = calcSP(detEtRateElc, detEtRateJet);
+            float hadEtEffic = calcSP(detHadEtRateElc, detHadEtRateJet);
             std::string dataLabel 
             pHypo->getDataLabel(dataLabel);
             line2.Form("T2Calo Efficience = %.4f", effic);
-            TText *p3 = line3.Form("Electrons Efficience = %.4f", detRateElc);
-            TText *p4 = line4.Form("Jets Efficience = %.4f", detRateJet);
-            p3->SetTextColor(4);
-            p4->SetTextColor(2);
-            line4.Form("rCore Cut Efficience = %.4f", detrCoreRate);
+            line3.Form("Electrons Efficience = %.4f", detRateElc);
+            TText *p2 = line4.Form("Jets Efficience = %.4f", detRateJet);
+            p1->SetTextColor(4);
+            p2->SetTextColor(2);
+            line4.Form("rCore Cut Efficience = %.4f", rCoreEffic);
+            line5.Form("rCore Electron Rate = %.4f", detrCoreRateElc);
+            line6.Form("rCore Jet Rate = %.4f", detrCoreRateJet);
             line7.Form("eRatio Cut Efficience = %.4f", deteRatioRate);
+            line5.Form("eRatio Electron Rate = %.4f", detrCoreRateElc);
+            line6.Form("rCore Jet Rate = %.4f", detrCoreRateJet);
             line5.Form("Et_{Em} Cut Efficience = %.4f", detEtRate);
             line6.Form("Et_{Had} Cut Efficience = %.4f", detHadEtRate;);
             pt->AddText("");
