15800be0cc25470419e1be82f50256abe2082d6c
wsfreund <wsfreund@wsfreund-laptop.(none)>
2010-02-18 21:33:16 +0100

    adicionado um cout para saber qual eh a dos pontos em zero

--- EtEtaPhi/HypoErrorsGraph.cxx
+++ EtEtaPhi/HypoErrorsGraph.cxx
@@ -150,8 +150,10 @@ HypoErrorsGraph::CODE HypoErrorsGraph::genEfficErrors(float* edges, float* pX, f
             if (regData!=0){
                 error = 1/TMath::Sqrt(regData)*100.;
                 *pX = (*edges) + HALF_REGION_SIZE;
-                if (dataLabel == "elc")
+                if (dataLabel == "elc"){
                     *effic = (float)regElectrons / (float)regData*100.;
+                    if (*effic <3) cout<<"Effic:"<<*effic<<"on position "<<lowEdge<<endl;
+                }
                 else if (dataLabel == "jet")
                     *effic = (float)regElectrons / (float)regData *100.;
                 checkAndGenErrors(*effic, error, *lowEdgeErrors, *hiEdgeErrors);
