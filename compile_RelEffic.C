17ba90e5bcbe9c5c3ebd58c992fc3a4b20eff1ce
wsfreund <wsfreund@wsfreund-laptop.(none)>
2010-02-23 02:03:31 +0100

    Modificado nomes só pela estetica

---
+++ compile_RelEffic.C
@@ -0,0 +1,8 @@
+{
+gROOT->ProcessLine(".L HypoBase.cxx++");
+gROOT->ProcessLine(".L T2CaCommon.cxx++");
+gROOT->ProcessLine(".L RelEfficTool/HypoErrorsGraph.cxx++");
+gROOT->ProcessLine(".L RelEfficTool/RelEfficBase.h++");
+gROOT->ProcessLine(".L RelEfficTool/T2CaRelEffic.cxx++");
+gROOT->ProcessLine(".L RelEfficTool/RelEfficCanvas.cxx++");
+}
--- load_EtEtaPhi.C
+++
@@ -1,8 +0,0 @@
-{
-gROOT->ProcessLine(".L HypoBase.cxx+");
-gROOT->ProcessLine(".L T2CaCommon.cxx+");
-gROOT->ProcessLine(".L EtEtaPhi/HypoErrorsGraph.cxx+");
-gROOT->ProcessLine(".L EtEtaPhi/RelEfficBase.h+");
-gROOT->ProcessLine(".L EtEtaPhi/T2CaRelEffic.cxx+");
-gROOT->ProcessLine(".L EtEtaPhi/RelEfficCanvas.cxx+");
-}
