be30d7c0fcd0890c22e442647215f6015e8d6646
wsfreund <wsfreund@wsfreund-laptop.(none)>
2010-04-02 23:01:40 +0200

    Adicionado arquivo para compilar tudo

---
+++ load_HypoIterator.C
@@ -0,0 +1,22 @@
+{
+gROOT->ProcessLine(".L HypoBase.cxx+");
+gROOT->ProcessLine(".L T2CaConfig.cxx+");
+gROOT->ProcessLine(".L T2CaBase.h+");
+gROOT->ProcessLine(".L T2CaCommon.cxx+");
+gROOT->ProcessLine(".L NeuralConfig.cxx+");
+gROOT->ProcessLine(".L NeuralBase.h+");
+gROOT->ProcessLine(".L Neural.cxx+");
+gROOT->ProcessLine(".L NeuralCommon.cxx+");
+gROOT->ProcessLine(".L DrawHypoVarTool/HypoVarHist.cxx+");
+gROOT->ProcessLine(".L DrawHypoVarTool/HypoVarBase.h+");
+gROOT->ProcessLine(".L DrawHypoVarTool/T2CaVarGraph.cxx+");
+gROOT->ProcessLine(".L DrawHypoVarTool/NeuralVarGraph.cxx+");
+gROOT->ProcessLine(".L DrawHypoVarTool/HypoVarCanvas.cxx+");
+gROOT->ProcessLine(".L HypoCompTool/HypoCompCanvas.cxx+");
+gROOT->ProcessLine(".L RelEfficTool/HypoErrorsGraph.cxx+");
+gROOT->ProcessLine(".L RelEfficTool/RelEfficBase.h+");
+gROOT->ProcessLine(".L RelEfficTool/T2CaRelEffic.cxx+");
+gROOT->ProcessLine(".L RelEfficTool/NeuralRelEffic.cxx+");
+gROOT->ProcessLine(".L RelEfficTool/RelEfficCanvas.cxx+");
+}
+
